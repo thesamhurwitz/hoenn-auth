76908d1f64e345e8adc31f96fac810e4fb809a9f
SamHurwitz <samhurwitz@mail.ru>
2022-03-24 03:12:33 +0500

    feat: add jest

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
+  moduleDirectories: ['node_modules', '.'],
+};
